eab360e02865e34a98e20e558ee3999199b5c846
KavyaPratap Singh <kavyapratapsingh221@gmail.com>
2025-04-20 19:40:06 +0530

    Update hackathon-landing-page.jsx

--- src/pages/hackathon-landing-page.jsx
+++ src/pages/hackathon-landing-page.jsx
@@ -5,7 +5,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import Masonry from 'react-masonry-css';
-
+import imagesLoaded from 'imagesloaded';
 import {
   Award,
   Calendar,
